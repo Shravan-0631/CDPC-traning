7b15357ce0378082680f25b727eccab4f2e1831c
Shravan Mungelwar <shravanmungelwar06@gmail.com>
2026-03-18 16:53:19 +0530

    Add files via upload

---
+++ pythod_day_8.py
@@ -0,0 +1,61 @@
+# class student:
+#     def showA(self,a,b):
+#         print("I am in showA")
+#     def showB(a):
+#         print("I am in showA")
+        
+        
+# if __name__ == '__main__':
+#     obj=student()
+#     obj.showA(11,22)
+#     student.showB(22)
+
+
+
+
+
+
+# two sum
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range (len(nums)-1):
+#             for j in range (i+1,len(nums)):
+#                 if target==nums[i]+nums[j]:
+#                      return[i,j]
+                
+                
+                
+                
+                
+                
+# fizz buzz        
+                
+# class Solution:
+#     def fizzBuzz(self, n: int) -> List[str]:
+#         ans=[]
+#         for i in range (1,n+1):
+#             if i%3==0 and i%5==0:
+#                ans.append("FizzBuzz")
+#             elif i%3==0:
+#                  ans.append("Fizz")
+#             elif i%5==0:
+#                  ans.append("Buzz")
+#             else:
+#                 ans.append(str(i))
+
+#         return ans
+
+
+
+
+
+# running sum of 1d array
+
+
+# class Solution:
+#     def runningSum(self, nums: List[int]) -> List[int]:
+#         ans=[]
+#         sum=0
+#         for i in range(1,len(nums)):
+#             nums[i]=nums[i]+nums[i-1]
+#         return nums
